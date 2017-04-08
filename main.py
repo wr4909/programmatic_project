8bad13d2b19906e63dd8ebec194acaa7561290cd
Yang Li <wr4909@gmail.com>
2017-04-07 19:59:22 -0500

    started, need to do posterior

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ import pandas as pd
 
 """
 Need to rank users(TDID) based on the probability of visiting the "Contact Us Page"
+Find P(contact-us page | tdid)
+P(tdid | contact-us page) ~= P(contact-us page | tdid) * P(tdid)
 """
+tag_dict = {'Contact Us' : 'qelg9wq', 'Products' : 'pq3g1hn', 'About': 'ipi5afe', 'News': 'yi0fkw5', 'TTD Site': 'wjl3e83'}
 
-broken_df = pd.read_csv('Programmatic Project_Scoring_TTD pixel fires.csv')
+df = pd.read_csv('Programmatic Project_Scoring_TTD pixel fires.csv')
 
 
 def main():
-    print type(broken_df)
-    print broken_df['country']
+    df['tdid_count'] = df.groupby('tdid')['tdid'].transform('count')
+    df['prior'] = df['tdid_count'] * 1.0 / len(df)
+
+    #TODO: posterior: out of all contact page access, what percent were accessed by this tdid
+    df['posterior'] = 1.0 * df.loc[df['trackingtagid'] == tag_dict['Contact Us'] & df['']].size() / df.loc[df['trackingtagid'] == tag_dict['Contact Us']].size)()
+    df['likelihood'] = df['posterior'] / df['prior']
+    df.sort('likelihood', ascending=False)
 
 if __name__ == '__main__':
     main()
