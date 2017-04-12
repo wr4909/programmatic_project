09dc22b6f225abc3c564f00316dc5a11b33a37a1
Zach Kimberg <zachary@kimberg.com>
2017-04-11 19:36:38 -0500

    Only last 90 days

--- main.py
+++ main.py
@@ -2,14 +2,15 @@
 
 import numpy as np
 import pandas as pd
+import datetime
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB
 
-df = pd.read_csv('Programmatic Project_Scoring_TTD pixel fires.csv', encoding='latin1')
+df = pd.read_csv('Programmatic Project_Scoring_TTD pixel fires.csv', encoding='latin1', parse_dates=['logentrytime'])
 
 # Remove duplicate tdid
 visited_tdids = df[df.trackingtagid.as_matrix() == 'qelg9wq']['tdid']
 df['contact'] = df.tdid.isin(visited_tdids)
-df = df.drop_duplicates('tdid', keep='first')
+df = df.sort_values('logentrytime', ascending=False).drop_duplicates('tdid', keep='first')
 
 Y = df['contact'].as_matrix()
 
@@ -33,5 +34,6 @@ nb.fit(X, Y)
 probs = nb.predict_proba(X)
 df['score'] = probs[:,1]
 
-result = df.sort_values('score', ascending=False)[:10000]
+daterange = datetime.date.today() - datetime.timedelta(days=90)
+result = df[df.logentrytime >= daterange].sort_values('score', ascending=False)[:10000]
 result[['tdid','score']].to_csv('output.csv', index=False)
