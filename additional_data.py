7d53ce515fb1772de720786e85c8e410656a50ed
akuznets0v <akuz.dev@gmail.com>
2017-04-26 00:12:39 -0500

    Added additional_data csv's, changed their column names, created untested script to load all csvs into a list of DataFrames, and a method to merge with a df from part 1

---
+++ additional_data.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import os
+
+"""
+Loads all csvs into a list of DataFrames
+Returns that list and a dictionary that can be used to call find each DataFrame by name.
+
+Sample Use:
+df_list, index_dict = load_all_csv()
+df_list[index_dict["ip"]]
+"""
+
+def load_all_csv(directory="/additional_data"):
+	cur_dir = os.getcwd()
+	target_directory = cur_dir + directory + "/"
+	files = [ f for f in os.listdir(target_directory) if os.path.isfile(os.path.join(target_directory,f)) ]
+	df_list = []
+	for file_csv in files:
+		df_list.append(read_csv(file_csv))
+	index_dict = {}
+	for indx, file_csv in enumerate(files):
+		index_dict[file_csv] = indx
+
+	return df_list, index_dicts
+
+"""
+Takes a normal DataFrame from part 1 (or a slice) and inner joins using the additional
+DataFrame, on a specified column from each.
+"""
+
+def inner_join(normal_df, additional_df, normal_df_column_name, additional_df_column_name):
+	return pd.merge(normal_df, additional_df, on =[normal_df_column_name, additional_df_column_name])
+
+if __name__ == "__main__":
+	pass
