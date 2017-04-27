1c300db7ad2a4cf4b9cadf70dcf1e43f92a7ac2f
akuznets0v <akuz.dev@gmail.com>
2017-04-27 12:12:21 -0500

    Added corrections to script

--- additional_data.py
+++ additional_data.py
@@ -16,12 +16,12 @@ def load_all_csv(directory="/additional_data"):
 	files = [ f for f in os.listdir(target_directory) if os.path.isfile(os.path.join(target_directory,f)) ]
 	df_list = []
 	for file_csv in files:
-		df_list.append(read_csv(file_csv))
+		df_list.append(pd.read_csv(target_directory + file_csv))
 	index_dict = {}
 	for indx, file_csv in enumerate(files):
 		index_dict[file_csv] = indx
 
-	return df_list, index_dicts
+	return df_list, index_dict
 
 """
 Takes a normal DataFrame from part 1 (or a slice) and inner joins using the additional
